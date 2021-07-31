c52d72a43a64bc6c87d25a8bfd3902364c26cfcf
geniuskouta <knakano95@gmail.com>
2021-08-01 01:21:52 +0900

    add error handling to greetings.go

--- src/102_create_a_go_module/hello/hello.go
+++ src/102_create_a_go_module/hello/hello.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Kouta")
+	log.SetPrefix("greetings: ") // print the command name ("greetings: ")
+    log.SetFlags(0) //  at the start of its log messages, without a time stamp or source file information.
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err) // print the error and stop the program
+	}
+
 	fmt.Println(message)
 }
