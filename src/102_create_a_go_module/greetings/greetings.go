aa4de0270f63aa5e5907aa0367a57c37eb002268
geniuskouta <knakano95@gmail.com>
2021-08-01 13:31:03 +0900

    add changes to greetings for the last commit

--- src/102_create_a_go_module/greetings/greetings.go
+++ src/102_create_a_go_module/greetings/greetings.go
@@ -1,14 +1,22 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 /* the first capital letter func is an exported name */
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name") //any Go function can return multiple values
+	}
+
+
 	/*
 	* initialize message variable in one line instead of the following:
 	* var message string
 	* message = fmt.Sprintf("Hi, %v. Welcome!", name)
 	*/
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil //nil (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded
 }
