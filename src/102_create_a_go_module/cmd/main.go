dcb4c8700204ec112350b81de06018662cf6c9df
koutactiyon <kouta.nakano@actiyon.com>
2024-11-10 18:47:10 +0900

    Use map

--- src/102_create_a_go_module/cmd/main.go
+++ src/102_create_a_go_module/cmd/main.go
@@ -9,11 +9,13 @@ import (
 func main() {
 	log.SetPrefix("greetings: ") // print the command name ("greetings: ")
 	log.SetFlags(0)              //  at the start of its log messages, without a time stamp or source file information.
-	message, err := greetings.Hello("Kouta")
+
+	names := []string{"Kouta", "Gill", "Kat"}
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err) // print the error and stop the program
 	}
 
-	fmt.Println(message)
+	fmt.Println(messages)
 }
